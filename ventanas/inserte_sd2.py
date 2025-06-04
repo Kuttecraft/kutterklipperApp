efe206727180434a60dff4fb335b66ab724d72c3
Kuttercraft <kuttercraft@gmail.com>
2025-06-04 20:39:07 -0300

    mejora menu sd

--- ventanas/inserte_sd2.py
+++ ventanas/inserte_sd2.py
@@ -105,6 +105,48 @@ class PantallaInserteSD2(tk.Frame):
         )
         return self.main_canvas
 
+    def create_text_sd_copiado(self):
+        # Crear un Canvas que abarque toda la ventana
+        self.main_canvas = tk.Canvas(self, bg='black', highlightthickness=0)
+        self.main_canvas.pack(fill='both', expand=True)
+
+        # Añadir la imagen de la impresora 3D
+        impresora_3d = cargar_imagen(RUTA_IMAGEN_IMPRESORA_3D, 640, 329)
+        if impresora_3d:
+            self.main_canvas.create_image(80, 8, anchor='nw', image=impresora_3d)
+            self.main_canvas.image = impresora_3d
+
+        # Añadir el texto sobre la imagen
+        self.main_canvas.create_text(
+            VENTANA_ANCHO // 2, 350,
+            text="Se copio el archivo \"firmware.bin\" a la SD",
+            font=('Montserrat', 18, 'bold'),
+            fill=COLOR_TEXTO,
+            justify='center'
+        )
+        return self.main_canvas
+
+    def create_text_sd_error(self):
+        # Crear un Canvas que abarque toda la ventana
+        self.main_canvas = tk.Canvas(self, bg='black', highlightthickness=0)
+        self.main_canvas.pack(fill='both', expand=True)
+
+        # Añadir la imagen de la impresora 3D
+        impresora_3d = cargar_imagen(RUTA_IMAGEN_IMPRESORA_3D, 640, 329)
+        if impresora_3d:
+            self.main_canvas.create_image(80, 8, anchor='nw', image=impresora_3d)
+            self.main_canvas.image = impresora_3d
+
+        # Añadir el texto sobre la imagen
+        self.main_canvas.create_text(
+            VENTANA_ANCHO // 2, 350,
+            text="Error al copiar el archivo \"firmware.bin\" a la SD\nSe tendra que installar manualmente el firmware",
+            font=('Montserrat', 18, 'bold'),
+            fill=COLOR_TEXTO,
+            justify='center'
+        )
+        return self.main_canvas
+
     def bind_events(self):
         self.bind_all('<Escape>', lambda e: self.master.quit())
 
@@ -133,7 +175,7 @@ class PantallaInserteSD2(tk.Frame):
                 RUTA_BOTON, 
                 "Continuar", 
                 276, 410,
-                command=lambda: self.seleccionar_opcion("Tipo", "8_Bits")
+                command=lambda: self.copiar_firmware()
             )
 
         except subprocess.CalledProcessError as e:
@@ -157,31 +199,68 @@ class PantallaInserteSD2(tk.Frame):
                 276, 410,
                 command=lambda: self.montar_sd()
             )
+    def copiar_firmware(self):
+        destino = os.path.join(self.sd_mount_point, "firmware.bin")
+        self.boton_sd.destroy()
+        self.texto_sd.destroy()
 
+        try:
+            if not os.path.exists(self.archivo_firmware):
+                print(f"[ERROR] El archivo de firmware no existe: {self.archivo_firmware}")
+                
+            # Copiar el archivo
+            subprocess.run(["sudo", "cp", self.archivo_firmware, destino], check=True)
+            print(f"[OK] Archivo copiado a {destino}")
+
+            
+
+            self.texto_sd = self.create_text_sd_copiado()
+            self.boton_sd = crear_boton(
+                self, 
+                RUTA_BOTON, 
+                "Continuar", 
+                276, 410,
+                command=lambda: self.seleccionar_opcion("Tipo", "8_Bits")
+            )
+
+        except PermissionError:
+            self.texto_sd = self.create_text_sd_error()
+            self.boton_sd = crear_boton(
+                self, 
+                RUTA_BOTON, 
+                "Continuar", 
+                276, 410,
+                command=lambda: self.continuar_callback()
+            )
+            print("[ERROR] Permiso denegado al copiar el archivo.")
+            
+        except shutil.SameFileError:
+            print("[ERROR] El archivo de origen y destino son iguales.")
+            self.texto_sd = self.create_text_sd_error()
+            self.boton_sd = crear_boton(
+                self, 
+                RUTA_BOTON, 
+                "Continuar", 
+                276, 410,
+                command=lambda: self.continuar_callback()
+            )
+            
+        except Exception as e:
+            print(f"[ERROR] Error inesperado al copiar el archivo: {e}")    
+            self.texto_sd = self.create_text_sd_error()
+            self.boton_sd = crear_boton(
+                self, 
+                RUTA_BOTON, 
+                "Continuar", 
+                276, 410,
+                command=lambda: self.continuar_callback()
+            )
 
 
 
 
 
 '''
-def copiar_firmware():
-    destino = os.path.join(sd_mount_point, "firmware.bin")
-    try:
-        if not os.path.exists(archivo_firmware):
-            print(f"[ERROR] El archivo de firmware no existe: {archivo_firmware}")
-            sys.exit(1)
-
-        subprocess.run(["sudo", "cp", archivo_firmware, destino], check=True)
-        print(f"[OK] Archivo copiado a {destino}")
-    except PermissionError:
-        print("[ERROR] Permiso denegado al copiar el archivo.")
-        sys.exit(1)
-    except shutil.SameFileError:
-        print("[ERROR] El archivo de origen y destino son iguales.")
-        sys.exit(1)
-    except Exception as e:
-        print(f"[ERROR] Error inesperado al copiar el archivo: {e}")
-        sys.exit(1)
 
 def desmontar_sd():
     print("[INFO] Desmontando la SD...")
