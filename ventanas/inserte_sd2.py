bece407902cfd05dbbf2670c5ef30e0b6ba4d97d
Kuttercraft <kuttercraft@gmail.com>
2025-06-04 20:47:41 -0300

    funcionalidad

--- ventanas/inserte_sd2.py
+++ ventanas/inserte_sd2.py
@@ -146,6 +146,27 @@ class PantallaInserteSD2(tk.Frame):
             justify='center'
         )
         return self.main_canvas
+    
+    def create_text_sd_desmontada(self):
+        # Crear un Canvas que abarque toda la ventana
+        self.main_canvas = tk.Canvas(self, bg='black', highlightthickness=0)
+        self.main_canvas.pack(fill='both', expand=True)
+
+        # Añadir la imagen de la impresora 3D
+        impresora_3d = cargar_imagen(RUTA_IMAGEN_IMPRESORA_3D, 640, 329)
+        if impresora_3d:
+            self.main_canvas.create_image(80, 8, anchor='nw', image=impresora_3d)
+            self.main_canvas.image = impresora_3d
+
+        # Añadir el texto sobre la imagen
+        self.main_canvas.create_text(
+            VENTANA_ANCHO // 2, 250,
+            text="La tarjeta SD se expulso correctamente\nProceda a insertar la tarjeta SD en la electronica\nPara su instalacion en la impresora 3D",
+            font=('Montserrat', 18, 'bold'),
+            fill=COLOR_TEXTO,
+            justify='center'
+        )
+        return self.main_canvas
 
     def bind_events(self):
         self.bind_all('<Escape>', lambda e: self.master.quit())
@@ -220,7 +241,7 @@ class PantallaInserteSD2(tk.Frame):
                 RUTA_BOTON, 
                 "Continuar", 
                 276, 410,
-                command=lambda: self.seleccionar_opcion("Tipo", "8_Bits")
+                command=lambda: self.desmontar_sd()
             )
 
         except PermissionError:
@@ -256,19 +277,24 @@ class PantallaInserteSD2(tk.Frame):
                 command=lambda: self.continuar_callback()
             )
 
+    def desmontar_sd(self):
+        print("[INFO] Desmontando la SD...")
+        try:
+            subprocess.run(["sudo", "umount", self.sd_mount_point], check=True)
+            print("[OK] SD desmontada correctamente.")
 
+            self.texto_sd.destroy()
+            self.boton_sd.destroy()
 
-
-
-'''
-
-def desmontar_sd():
-    print("[INFO] Desmontando la SD...")
-    try:
-        subprocess.run(["sudo", "umount", sd_mount_point], check=True)
-        print("[OK] SD desmontada correctamente.")
-    except subprocess.CalledProcessError as e:
-        print(f"[ERROR] Fallo al desmontar la SD: {e}")
-    except Exception as e:
-        print(f"[ERROR] Error inesperado al desmontar la SD: {e}")
-'''
+            self.texto_sd = self.create_text_sd_desmontada()
+            self.boton_sd = crear_boton(
+                self, 
+                RUTA_BOTON, 
+                "Continuar", 
+                276, 410,
+                command=lambda: self.continuar_callback()
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"[ERROR] Fallo al desmontar la SD: {e}")
+        except Exception as e:
+            print(f"[ERROR] Error inesperado al desmontar la SD: {e}")
